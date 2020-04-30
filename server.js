7e4433531024a5975dbe8f185e79da02fbca913d
IndimaDias <indimad@yahoo.com>
2020-04-30 12:07:41 -0400

    change folder strcture

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("/sa/build"));
+  }
+  
+const uri = process.env.ATLAS_URI;
+console.log(uri);
+mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex: true });
+// .catch(err => {
+//     console.error('App starting error:', err.stack);
+// });
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const businessRouter  = require('./routes/business');
+const imageRouter  = require('./routes/Image');
+
+app.use('/business', businessRouter);
+app.use('/image', imageRouter);
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
+
+//ATLAS_URI=mongodb+srv://bswainbank:llamazmdb1@cluster0-fgdma.mongodb.net/test?retryWrites=true&w=majority
