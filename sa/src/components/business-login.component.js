e6dee6f5da2b2ecaa6cb1a8079c34f9fa2c302ec
IndimaDias <indimad@yahoo.com>
2020-05-04 15:12:59 -0400

    test path

--- sa/src/components/business-login.component.js
+++ sa/src/components/business-login.component.js
@@ -51,8 +51,8 @@ export default class BusinessLogin extends Component {
             .then(res=> {
                 console.log(res.data);
                 //  window.location = 'edit/' + res.data;
-               window.open("/edit" + res.data, "_self");
-               
+               //window.open("/edit/" + res.data, "_self");
+                   window.location.assign('/edit/'+ res.data);
             //   this.props.history.push('/edit/'+ res.data); // <--- The page you want to redirect your user to.
 
             })
